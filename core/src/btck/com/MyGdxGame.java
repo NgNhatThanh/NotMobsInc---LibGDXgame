ab9d4f00389e083c3162e89b6236a0ed4e193684
Nhật Thành <127038782+NgNhatThanh@users.noreply.github.com>
2024-04-29 14:38:33 +0700

    Revert "Basic menu"

--- core/src/btck/com/MyGdxGame.java
+++ core/src/btck/com/MyGdxGame.java
@@ -1,25 +1,31 @@
 package btck.com;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import screens.MainMenuScreen;
 
-import java.awt.*;
-
-public class MyGdxGame extends Game {
-	public SpriteBatch batch;
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		this.setScreen(new MainMenuScreen(this));
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		super.render();
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
 	}
 }
